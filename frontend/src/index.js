e6f476cf393a554c5215fb692d33c5531387816e
Abdur Razzak <73746963+abdur-razzak2672@users.noreply.github.com>
2022-08-11 02:50:45 +0600

    User Authentication functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import './index.css';
-// import './bootstrap.min.css';
+import './bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+<link rel="stylesheet" type="text/css" href="https://dtt1c9id3txwq.cloudfront.net/themes/16195/assets/stylesheets/manifest-main-c3a0da30d701eb2e1132b7821b8de24bddbe06a0471e45267cb2f361f0a7cfa3.css?1657951192" />
+
 
 
 ReactDOM.render(
