ae3a2168d8f33db46b935eec7bf78a2f7db4b4d4
Abdur Razzak <73746963+abdur-razzak2672@users.noreply.github.com>
2022-08-11 15:51:57 +0600

    Private Routr

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view ,permission_classes
+from rest_framework.permissions import IsAuthenticated , IsAdminUser
 from .models import Product
 from .serializers import ProductSerializer ,UserSerializer ,UserSerializerWithToken
 #from .products import products
@@ -35,6 +36,7 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUsersProfile(request):
     user = request.user
     serializer = UserSerializer(user , many=False)
