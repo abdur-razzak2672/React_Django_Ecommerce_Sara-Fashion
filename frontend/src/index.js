8e406fbaac92177595f2d4aa06bd614d8d060493
Abdur Razzak <73746963+abdur-razzak2672@users.noreply.github.com>
2022-08-17 18:53:31 +0600

    work in navbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,8 @@ import './index.css';
 import './bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+ import 'bootstrap/dist/css/bootstrap.min.css';
+ 
 <link rel="stylesheet" type="text/css" href="https://dtt1c9id3txwq.cloudfront.net/themes/16195/assets/stylesheets/manifest-main-c3a0da30d701eb2e1132b7821b8de24bddbe06a0471e45267cb2f361f0a7cfa3.css?1657951192" />
 
 
